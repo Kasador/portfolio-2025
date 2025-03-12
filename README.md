4cd91d813a00f84656c9f1fdbcae9d4df9f8c37a
Kasador <hunterstevenshaw@gmail.com>
2025-03-12 18:20:47 -0500

    🆕 Added Firebase Storage for videos/images >>> .env file to hide API keys/information.

--- README.md
+++ README.md
@@ -58,6 +58,9 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-### 🛠️ 🐛 **Issues/Bugs**
-
 ### 📈 📸 **Progress/Screenshots**
+
+# Added Firebase Storage and loaded in main videos for both desktop/mobile
+
+<img width="297" alt="Image" src="https://github.com/user-attachments/assets/5bb63819-4c62-4797-a98e-ba8a51d99c2e" />
+<img width="700" alt="Image" src="https://github.com/user-attachments/assets/594b453c-07d0-456c-a75f-aa9ba3c4d8d7" />
--- src/components/desktop/Desktop.jsx
+++ src/components/desktop/Desktop.jsx
@@ -1,14 +1,21 @@
-// import react from 'react'
-// import './App.scss'
+import useFirebaseStorage from "@/hooks/useFirebaseStorage";
 
 function Desktop() {
-//   const [count, setCount] = useState(0)
+  const { url, loading, error } = useFirebaseStorage("desktop/videos/Desktop.webm");
+
+  if (loading) return <p>Loading video...</p>;
+  if (error) return <p>Error loading boot animation: {error}</p>;
 
   return (
-    <>
-     <h1>Desktop</h1>
-    </>
-  )
+    <div className="video-wrapper">
+      {url && (
+        <video autoPlay muted loop id="desktop-video">
+          <source src={url} type="video/webm" />
+          Your browser does not support the video tag.
+        </video>
+      )}
+    </div>
+  );
 }
 
 export default Desktop;
