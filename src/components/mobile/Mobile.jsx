e11008f8094ef126eef231c4f37af9ed4efae7d7
Kasador <hunterstevenshaw@gmail.com>
2025-03-13 10:02:22 -0700

    🛠️ Moved background animation videos (desktop/mobile) to root folder to faster load times instead of firebase storage.

--- src/components/mobile/Mobile.jsx
+++ src/components/mobile/Mobile.jsx
@@ -1,19 +1,24 @@
-import useFirebaseStorage from "@/hooks/useFirebaseStorage";
+// import useFirebaseStorage from "@/hooks/useFirebaseStorage";
 
 function Mobile() {
-  const { url, loading, error } = useFirebaseStorage("mobile/videos/Mobile_v5_Slow.mp4");
+  // const { url, loading, error } = useFirebaseStorage("mobile/videos/Mobile.mp4");
 
-  if (loading) return <p>Loading video...</p>;
-  if (error) return <p>Error loading boot animation: {error}</p>;
+  // if (loading) return <p>Loading video...</p>;
+  // if (error) return <p>Error loading boot animation: {error}</p>;
 
   return (
     <div className="video-wrapper">
-      {url && (
-        <video autoPlay muted loop playsInline id="mobile-video">
-          <source src={url} type="video/mp4" />
+      <video   
+        autoPlay 
+        muted 
+        playsInline 
+        preload="auto"
+        id="mobile-video"
+        onCanPlay={(e) => e.target.play()}
+        onError={(e) => console.error("Video Error:", e)}>
+          <source src="/videos/Mobile.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-      )}
     </div>
   );
 }
