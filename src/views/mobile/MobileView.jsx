7bfc878924bbf97521727631018bb673be001283
Kasador <hunterstevenshaw@gmail.com>
2025-03-13 19:28:52 -0700

    🚨 Screen ratio for fake screens >>> Switching to desktop.

---
+++ src/views/mobile/MobileView.jsx
@@ -0,0 +1,11 @@
+import '../../styles/MobileView.scss'
+
+const MobileView = () => {
+    return (
+        <section id="mobile-view">
+            <h1>Mobile</h1>
+        </section>
+    )
+}
+
+export default MobileView;
